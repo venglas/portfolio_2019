9557bc7bbf8abd7f878f27ef312be735cbc57687
Venglas <bartek.bober97@gmail.com>
2019-02-05 00:09:34 +0100

    set basic canvas

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,9 +1,36 @@
 <template>
-    <canvas></canvas>
+    <canvas ></canvas>
 </template>
 
 <script>
 export default {
-  name: 'Background'
+  name: 'Background',
+  data: () => {
+    return {
+        test: "hello"
+    }
+  },
+  methods: {
+  },
+  mounted () {
+    const canvas = document.querySelector('canvas');
+    const c = canvas.getContext('2d');
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    c.beginPath();
+    c.arc(200, 200, 30, Math.PI*2, false);
+    c.stroke();
+
+  }
 }
 </script>
+
+<style>
+    canvas{
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+</style>
