9312abd9e802eaeeadba46183996a246593b9320
Venglas <bartek.bober97@gmail.com>
2019-02-05 05:37:26 +0100

    add smooth moving and change object color after collision with wall

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -59,15 +59,15 @@ export default {
             y: rand(window.innerHeight),
             xlength: randFromTo(-20, window.innerWidth + 20),
             ylength: randFromTo(-20, window.innerHeight + 20),
-            xspeed: .5,
-            yspeed: .2,
-            colors: ['#C7E9F3', '#B3D1DA', '#8FA7AE'],
+            xspeed: .2,
+            yspeed: .3,
+            // colors: ['#C7E9F3', '#B3D1DA', '#8FA7AE'],
+            colors: ['rgba(0, 255, 255, 0.4)', 'rgba(0, 255, 197, 0.4)', 'rgba(0, 89, 197, 0.4)'],
             colorNumber: 0, // must be number of colors.length - 1
-            howMuchLines: 500,
+            howMuchLines: 80,
         }
         return info;
     }
-    let speed = 5;
 
     let lines = [];
     for (let i = 0; i < createLinesInfo().howMuchLines; i++){
@@ -87,36 +87,35 @@ export default {
             el.y += el.yspeed;
             
             if ( el.x >= window.innerWidth ) {
+                el.colorNumber = randFromTo(0, 3)
                 el.xspeed = -el.xspeed;
             } else if ( el.x <= 0 ) {
+                el.colorNumber = randFromTo(0, 3)
                 el.xspeed = -el.xspeed;
             }
 
             if ( el.y >= window.innerHeight ) {
+                el.colorNumber = randFromTo(0, 3)
                 el.yspeed = -el.yspeed;
             } else if ( el.y <= 0 ) {
+                el.colorNumber = randFromTo(0, 3)
                 el.yspeed = -el.yspeed;
             }
-
             
 
-            // if ( el.x >= window.innerWidth ) {
-            //     el.x -= 5;
-            // }
-
-            
             c.beginPath();
             c.moveTo(el.x, el.y);
-            c.lineTo(el.xlength, el.ylength);
 
-            // c.fillStyle = el.colors[el.colorNumber];
+            c.lineTo(el.xlength, el.ylength);
+            c.lineTo(el.xlength + 5, el.ylength + 30);
+            c.lineTo(el.xlength + 55, el.ylength + 35);
+
+            c.fillStyle = el.colors[el.colorNumber];
             c.strokeStyle = el.colors[el.colorNumber];
-            // c.fill();
+            c.fill();
             c.stroke();
         })
-    }
-
-       
+    }  
 
     createLines();
   }
@@ -128,5 +127,6 @@ export default {
         position: absolute;
         top: 0;
         left: 0;
+        background-color: rgba(0, 65, 90, .8);
     }
 </style>
