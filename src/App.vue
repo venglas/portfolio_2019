ad5bc0ff7a75c58617e60375947f78c5eadc612b
Venglas <bartek.bober97@gmail.com>
2019-02-05 07:07:13 +0100

    add new background from external source

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,9 @@ export default {
   components: {Background}
 }
 </script>
+
+<style>
+  #app {
+    background: url('https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+  }
+</style>
--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,5 +1,4 @@
 <template>
-    <canvas ></canvas>
 </template>
 
 <script>
@@ -15,114 +14,231 @@ export default {
   methods: {},
 
   mounted () {
-    const canvas = document.querySelector('canvas');
-    const c = canvas.getContext('2d');
+    const { PI, cos, sin, abs, sqrt, pow, round, random, atan2 } = Math;
+    const HALF_PI = 0.5 * PI;
+    const TAU = 2 * PI;
+    const TO_RAD = PI / 180;
+    const floor = n => n | 0;
+    const rand = n => n * random();
+    const randIn = (min, max) => rand(max - min) + min;
+    const randRange = n => n - rand(2 * n);
+    const fadeIn = (t, m) => t / m;
+    const fadeOut = (t, m) => (m - t) / m;
+    const fadeInOut = (t, m) => {
+        let hm = 0.5 * m;
+        return abs((t + hm) % m - hm) / (hm);
+    };
+    const dist = (x1, y1, x2, y2) => sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    const angle = (x1, y1, x2, y2) => atan2(y2 - y1, x2 - x1);
+    const lerp = (n1, n2, speed) => (1 - speed) * n1 + speed * n2;
 
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    c.fillStyle = 'red';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    const particleCount = 450;
+    const particlePropCount = 9;
+    const particlePropsLength = particleCount * particlePropCount;
+    const baseTTL = 100;
+    const rangeTTL = 500;
+    const baseSpeed = 0.15;
+    const rangeSpeed = 1;
+    const baseSize = 2;
+    const rangeSize = 15;
+    const baseHue = 20;
+    const rangeHue = 100;
+    const noiseSteps = 2;
+    const xOff = 0.0025;
+    const yOff = 0.005;
+    const zOff = 0.0005;
+    const backgroundColor = 'hsla(60,50%,3%,1)';
 
+    let container;
+    let canvas;
+    let ctx;
+    let center;
+    let gradient;
+    let tick;
+    let particleProps;
+    let positions;
+    let velocities;
+    let lifeSpans;
+    let speeds;
+    let sizes;
+    let hues;
 
-    let cursor = {
-        x: 0,
-        y: 0
+    function setup() {
+        createCanvas();
+        resize();
+        initParticles();
+        draw();
     }
-    canvas.addEventListener('mousemove', (ev)=>{
-        cursor.x = ev.clientX;
-        cursor.y = ev.clientY;
-    })
 
-    function animate () {
-    }
+    function initParticles() {
+        tick = 0;
+        particleProps = new Float32Array(particlePropsLength);
 
-    function rand(how) {
-        return Math.random() * how;
-    }
-
-    function randFromTo(from, to){
-        return Math.floor(Math.random() * to) + from;
-    }
-
-    function createLinesInfo () {
-        let info = {
-            x: rand(window.innerWidth),
-            y: rand(window.innerHeight),
-            xlength: randFromTo(-20, window.innerWidth + 20),
-            ylength: randFromTo(-20, window.innerHeight + 20),
-            xspeed: .4,
-            yspeed: .6,
-            // colors: ['#C7E9F3', '#B3D1DA', '#8FA7AE'],
-            colors: ['rgba(0, 255, 255, 0.4)', 'rgba(0, 255, 197, 0.4)', 'rgba(0, 89, 197, 0.4)'],
-            colorsDarkness: ['rgba(0, 255, 255, 0.8)', 'rgba(0, 255, 197, 0.8)', 'rgba(0, 89, 197, 0.8)'],
-            colorNumber: 0, // must be number of colors.length - 1
-            howMuchLines: 12,
+        let i;
+        
+        for (i = 0; i < particlePropsLength; i += particlePropCount) {
+            initParticle(i);
         }
-        return info;
     }
 
-    let lines = [];
-    for (let i = 0; i < createLinesInfo().howMuchLines; i++){
-        let linesInfo = createLinesInfo();
-        linesInfo.colorNumber = randFromTo(0, 3);
-        lines.push(linesInfo);
+    function initParticle(i) {
+        let theta, x, y, vx, vy, life, ttl, speed, size, hue;
+
+        x = rand(canvas.a.width);
+        y = rand(canvas.a.height);
+        theta = angle(x, y, center[0], center[1]);
+        vx = cos(theta) * 6;
+        vy = sin(theta) * 6;
+        life = 0;
+        ttl = baseTTL + rand(rangeTTL);
+        speed = baseSpeed + rand(rangeSpeed);
+        size = baseSize + rand(rangeSize);
+        hue = baseHue + rand(rangeHue);
+
+        particleProps.set([x, y, vx, vy, life, ttl, speed, size, hue], i);
     }
 
+    function drawParticles() {
+        let i;
 
-    function createLines() {
-        requestAnimationFrame(createLines);
-        c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        for (i = 0; i < particlePropsLength; i += particlePropCount) {
+            updateParticle(i);
+        }
+    }
 
-        lines.forEach(el => {
-            window.addEventListener('click', () => {
-                el.colorNumber = randFromTo(0, 3);
-            })
-            
-            el.x += el.xspeed;
-            el.y += el.yspeed;
-            
-            if ( el.x >= window.innerWidth ) {
-                el.colorNumber = randFromTo(0, 3)
-                el.xspeed = -el.xspeed;
-            } else if ( el.x <= 0 ) {
-                el.colorNumber = randFromTo(0, 3)
-                el.xspeed = -el.xspeed;
-            }
+    function updateParticle(i) {
+        let i2=1+i, i3=2+i, i4=3+i, i5=4+i, i6=5+i, i7=6+i, i8=7+i, i9=8+i;
+        let x, y, theta, vx, vy, life, ttl, speed, x2, y2, size, hue;
 
-            if ( el.y >= window.innerHeight ) {
-                el.colorNumber = randFromTo(0, 3)
-                el.yspeed = -el.yspeed;
-            } else if ( el.y <= 0 ) {
-                el.colorNumber = randFromTo(0, 3)
-                el.yspeed = -el.yspeed;
-            }
-            
+        x = particleProps[i];
+        y = particleProps[i2];
+        theta = angle(x, y, center[0], center[1]) + 0.75 * HALF_PI;
+        vx = lerp(particleProps[i3], 2 * cos(theta), 0.05);
+        vy = lerp(particleProps[i4], 2 * sin(theta), 0.05);
+        life = particleProps[i5];
+        ttl = particleProps[i6];
+        speed = particleProps[i7];
+        x2 = x + vx * speed;
+        y2 = y + vy * speed;
+        size = particleProps[i8];
+        hue = particleProps[i9];
 
-            c.beginPath();
-            c.moveTo(el.x, el.y);
+        drawParticle(x, y, theta, life, ttl, size, hue);
 
-            c.lineTo(el.xlength, el.ylength);
-            c.lineTo(el.xlength + 5, el.ylength + 30);
-            c.lineTo(el.xlength + 55, el.ylength + 35);
+        life++;
 
-            c.fillStyle = el.colors[el.colorNumber];
-            c.strokeStyle = el.colorsDarkness[el.colorNumber];
-            c.fill();
-            c.stroke();
-        })
-    }  
+        particleProps[i] = x2;
+        particleProps[i2] = y2;
+        particleProps[i3] = vx;
+        particleProps[i4] = vy;
+        particleProps[i5] = life;
 
-    createLines();
+        life > ttl && initParticle(i);
+    }
+
+    function drawParticle(x, y, theta, life, ttl, size, hue) {
+        let xRel = x - (0.5 * size), yRel = y - (0.5 * size);
+        
+        ctx.a.save();
+        ctx.a.lineCap = 'round';
+        ctx.a.lineWidth = 1;
+        ctx.a.strokeStyle = `hsla(${hue},100%,60%,${fadeInOut(life, ttl)})`;
+        ctx.a.beginPath();
+        ctx.a.translate(xRel, yRel);
+        ctx.a.rotate(theta);
+        ctx.a.translate(-xRel, -yRel);
+        ctx.a.strokeRect(xRel, yRel, size, size);
+        ctx.a.closePath();
+        ctx.a.restore();
+    }
+
+    function createCanvas() {
+        container = document.querySelector('#app');
+            canvas = {
+                a: document.createElement('canvas'),
+                b: document.createElement('canvas')
+            };
+            canvas.b.style = `
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            `;
+            container.appendChild(canvas.b);
+            ctx = {
+                a: canvas.a.getContext('2d'),
+                b: canvas.b.getContext('2d')
+        };
+        center = [];
+    }
+
+    function resize() {
+        const { innerWidth, innerHeight } = window;
+        
+        canvas.a.width = innerWidth;
+        canvas.a.height = innerHeight;
+
+        ctx.a.drawImage(canvas.b, 0, 0);
+
+        canvas.b.width = innerWidth;
+        canvas.b.height = innerHeight;
+    
+        ctx.b.drawImage(canvas.a, 0, 0);
+
+        center[0] = 0.5 * canvas.a.width;
+        center[1] = 0.5 * canvas.a.height;
+    }
+
+    function renderGlow() {
+        ctx.b.save();
+        ctx.b.filter = 'blur(8px) brightness(200%)';
+        ctx.b.globalCompositeOperation = 'lighter';
+        ctx.b.drawImage(canvas.a, 0, 0);
+        ctx.b.restore();
+
+        ctx.b.save();
+        ctx.b.filter = 'blur(4px) brightness(200%)';
+        ctx.b.globalCompositeOperation = 'lighter';
+        ctx.b.drawImage(canvas.a, 0, 0);
+        ctx.b.restore();
+    }
+
+    function render() {
+        ctx.b.save();
+        ctx.b.globalCompositeOperation = 'lighter';
+        ctx.b.drawImage(canvas.a, 0, 0);
+        ctx.b.restore();
+    }
+
+    function draw() {
+        tick++;
+
+        ctx.a.clearRect(0, 0, canvas.a.width, canvas.a.height);
+
+        ctx.b.fillStyle = backgroundColor;
+        
+        ctx.b.fillRect(0, 0, canvas.a.width, canvas.a.height);
+
+        drawParticles();
+        renderGlow();
+        render();
+
+        window.requestAnimationFrame(draw);
+    }
+
+    setup();
+    window.addEventListener('resize', resize);
   }
 }
 </script>
 
 <style>
-    canvas{
+    /* canvas{
         position: absolute;
         top: 0;
         left: 0;
         background-color: rgba(0, 65, 90, .8);
         opacity: .7;
-    }
+    } */
 </style>
