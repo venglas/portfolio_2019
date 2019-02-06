b385c8d295a7a25cbf5314bc47938ecd2de33c1f
Venglas <bartek.bober97@gmail.com>
2019-02-06 12:41:04 +0100

    add showing, hiding and toggle menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,8 +1,8 @@
 <template>
     <header class="header header--page">
         <nav class="nav nav--page" v-show="menu.show">
-            <ul class="list list--page-menu" id="menu">
-                <li class="list__item" v-for="items in menu.items">
+            <ul class="list list--page-menu" id="menu" v-bind:class="{hideHorizontalMenu : menu.hideMenu, leftSideMenu : menu.leftSideMenu}">
+                <li class="list__item" v-for="items in menu.items" v-on:click="hideMenu">
                     <h1>{{items}}</h1>
                 </li>
             </ul>
@@ -22,7 +22,9 @@ export default {
     return {
         menu: {
             items: ['start', 'projects', 'about', 'cv'],
-            show: false
+            show: false,
+            hideMenu: false,
+            leftSideMenu: false
         },
         startButton: {
             show: true,
@@ -37,12 +39,27 @@ export default {
         this.startButton.hideStartButtonClass = true;
         setTimeout(() => {
             this.startButton.show = false;
-        }, 1500); // 1500 cuz it's animation time
+        }, 1600); // 1500 cuz it's animation time
+    },
+    hideMenu(){
+        this.menu.hideMenu = true;
+        setTimeout(() => {
+            this.menu.leftSideMenu = true;
+            this.menuAutoToggle();
+        }, 1000)
+    },
+    menuAutoToggle(){
+        setTimeout(() => {
+            document.querySelector('#menu').style.left = '0px';
+            setTimeout(() => {
+                document.querySelector('#menu').style.left = '-90px';
+            }, 3000);
+        }, 200);
     }
   },
 
   mounted () {
-
+    // document.querySelector('#menu').style.transition = 'all ease 1000ms';
   }
 }
 </script>
