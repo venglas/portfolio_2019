bf6f09a2ab160d36a377a131d2f76bb8406bb6e9
Venglas <bartek.bober97@gmail.com>
2019-02-15 19:04:22 +0100

    add animated header of view

--- src/components/ViewHeader.vue
+++ src/components/ViewHeader.vue
@@ -1,7 +1,15 @@
 <template>
-    <h2>
-        {{text}}
-    </h2>
+    <div class="view-header">
+        <h2 v-show="!prop.text">
+            {{text}}
+        </h2>
+
+        <h2 v-show="prop.text">
+            <span v-for="letter in prop.text" class="view-header__letter" v-on:click="change_color">
+                {{letter}}
+            </span>
+        </h2>
+    </div>
 </template>
 
 <script>
@@ -10,9 +18,47 @@ export default {
   props: ['text'],
   data: () => {
     return {
-      
+      prop: {
+        text: false
+      }
     }
   },
+
+  created(){
+    let letters = [];
+    const scope = this;
+
+    for (let i = 0; i < this.text.length; i++){
+        letters.push(this.text[i]);
+    }
+
+    this.prop.text = letters;
+  },
+
+  methods: {
+      change_color(event){
+        event.target.classList.toggle('anim');
+      }
+  },
+
   mounted(){}
 }
 </script>
+
+<style lang="scss">
+    .anim{
+        animation-name: letter_jump;
+        animation-duration: 200ms;
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes letter_jump {
+        0% {
+            transform: rotate(0deg)
+        }
+
+        100% {
+            transform: rotate(180deg)
+        }
+    }
+</style>
