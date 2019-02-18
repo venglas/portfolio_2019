0d4fd6fe40ad7f6cc447406f565e84923e0b5169
Venglas <bartek.bober97@gmail.com>
2019-02-18 23:39:44 +0100

    put correct info in new project component

--- src/views/projects/canvasPongGame.vue
+++ src/views/projects/canvasPongGame.vue
@@ -1,12 +1,12 @@
 <template>
     <article class="view view--project" v-bind:style="{marginLeft: margin_left}">
-      <ViewHeader text="PRO8L3M Virtual CD"></ViewHeader>
+      <ViewHeader text="Canvas pong game"></ViewHeader>
 
       <GoBackButton></GoBackButton>
 
       <article class="section section--project">
         <header class="header">
-            PO8L3M virtual CD jest to projekt inspirowany krąkiem muzycznym "Ground zero" duetu PRO8L3M.
+            Canvas pong game jest prostą grą napisaną w javaScript przy uyciu canvas.
         </header>
 
         <section class="content">
@@ -21,7 +21,6 @@
             </ProjectSlider>
 
             <div class="content__description">
-                PO8L3M virtual CD jest to projekt wirtualnej płyty muzycznej, która zawiera 27 utworów wraz z tekstami. <br>
                 Technologie jakie zostały użyte w projekcie to m.in: 
                 <ul class="list">
                     <li class="list__item">
@@ -32,16 +31,6 @@
                     <li class="list__item">
                         <a href="https://www.ecma-international.org/ecma-262/6.0/" target="_blank">ES6</a>
                         <img src="../../assets/img/icons/js.png" alt="javascript icon" class="img icon">
-                    </li>
-
-                    <li class="list__item">
-                        <a href="https://sass-lang.com/" target="_blank">SCSS</a>
-                        <img src="../../assets/img/icons/sass.png" alt="javascript icon" class="img icon">
-                    </li>
-
-                    <li class="list__item">
-                        <a href="https://parceljs.org/" target="_blank">Parcel</a>
-                        <img src="../../assets/img/icons/parcel.png" alt="javascript icon" class="img icon">
                     </li>
                 </ul>
             </div>
