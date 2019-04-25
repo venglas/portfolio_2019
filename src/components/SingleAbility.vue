ec9848373055d70fc7438a32bf46109c70568ced
Venglas <bartek.bober97@gmail.com>
2019-04-25 16:57:06 +0200

    some fixes in single cv ability component

--- src/components/SingleAbility.vue
+++ src/components/SingleAbility.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="single-ability">
+  <div class="single-ability" v-bind:data-title="description">
     <label for="ability" class="single-ability__name">{{ability_name}}</label>
     <progress id="ability" max="100" v-bind:value="value" class="single-ability__progress-bar"> {{value}}% </progress>
   </div>
@@ -9,13 +9,17 @@
 
 export default {
   name: 'SingleAbility',
-  props: ["ability_name", "ability_value"],
+  props: ["ability_name", "ability_value", "description"],
   data: () => {
     return {
       value: 0
     }
   },
   mounted(){
+    if (this.description === '') {
+      console.log(this.$el.style.after)
+    }
+
     setTimeout(() => {
       this.value = this.ability_value;
     }, 1000);
