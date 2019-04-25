d9090a2979c3914b3eac70b687187a0f050ac37f
Venglas <bartek.bober97@gmail.com>
2019-04-25 15:43:21 +0200

    change appearance for progress bar

--- src/components/SingleAbility.vue
+++ src/components/SingleAbility.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="single-ability">
     <label for="ability" class="single-ability__name">{{ability_name}}</label>
-    <progress id="ability" max="100" v-bind:value="ability_value" class="single-ability__value"> {{ability_value}}% </progress>
+    <progress id="ability" max="100" v-bind:value="ability_value" class="single-ability__progress-bar"> {{ability_value}}% </progress>
   </div>
 </template>
 
