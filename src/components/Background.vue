0ccaae71cc8242d2c0a95652685c797376d28f1d
Venglas <bartek.bober97@gmail.com>
2019-04-29 21:51:31 +0200

    fix canvas mobile bg

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -160,8 +160,8 @@ export default {
                 position: absolute;
                 top: 0;
                 left: 0;
-                width: 100%;
-                height: 100%;
+                width: 100vw;
+                height: 100vh;
                 z-index: -100
             `;
             container.appendChild(canvas.b);
