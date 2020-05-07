f82da73602ff7ec5786c0d3cc3e927c273360dc3
Bartek <bartek.bober.koder@gmail.com>
2020-05-07 19:14:41 +0200

    reorder projects

--- src/views/projekty.vue
+++ src/views/projekty.vue
@@ -8,6 +8,43 @@
 
     <section class="section section--projects">
       <SingleProject
+        project_name="IG Helper Tool"
+        project_description="IG Helper Tool to narzędzie, które pozwoli Ci łatwiej zarządzać Twoim profilem na portalu instagram oraz zdobyć większą ilość osób, które obserwują Twój profil."
+        img_1="../../static/img/projects/ig-helper-tool/slide1.jpg"
+        img_2="../../static/img/projects/ig-helper-tool/slide2.jpg"
+        project_route="/ig-helper-tool"
+        project_live_link="https://github.com/venglas/ig-helper"
+        project_code_link="https://github.com/venglas/ig-helper"
+      >
+        <ProjectTag type="inProgress">in progress</ProjectTag>
+      </SingleProject>
+
+      <SingleProject
+        project_name="NBA Hall Of Hate"
+        project_description="Aplikacja webowa mająca na celu zbieranie opini, oraz w pewnym sensie gra społeczna jak i sieć łącząca fanów NBA :)"
+        img_1="../../static/img/projects/nba-hall-of-hate/slide1.jpg"
+        img_2="../../static/img/projects/nba-hall-of-hate/slide2.jpg"
+        project_route="/nba-hall-of-hate"
+        project_live_link="https://lucid-wright-54f76e.netlify.com"
+        project_code_link="https://github.com/venglas/nba-hall-of-hate-front"
+        project_code_link2="https://github.com/venglas/nba-hall-of-hate"
+      >
+        <ProjectTag type="inProgress">in progress</ProjectTag>
+      </SingleProject>
+      
+      <SingleProject
+        project_name="Margonem Multi Bot"
+        project_description="Margonem Multi Bot jest to projekt exp bota do gry internetowej margonem. Projekt jest realizowany jako rozszerzenie do przeglądarki chrome."
+        img_1="../../static/img/projects/margonem-multi-bot/slide4.jpg"
+        img_2="../../static/img/projects/margonem-multi-bot/slide1.jpg"
+        project_route="/margonem-multi-bot"
+        project_live_link="https://margonembot.wordpress.com/"
+        project_code_link="https://github.com/venglas/margonem-bot"
+      >
+        <ProjectTag type="closed">desolate</ProjectTag>
+      </SingleProject>
+
+      <SingleProject
         project_name="PRO8L3M virtual CD"
         project_description="PO8L3M virtual CD jest to projekt inspirowany krąkiem muzycznym 'Ground zero' duetu PRO8L3M."
         img_1="../../static/img/projects/problem-virtual-cd/slide2.jpg"
@@ -44,18 +81,6 @@
       </SingleProject>
 
       <SingleProject
-        project_name="Margonem Multi Bot"
-        project_description="Margonem Multi Bot jest to projekt exp bota do gry internetowej margonem. Projekt jest realizowany jako rozszerzenie do przeglądarki chrome."
-        img_1="../../static/img/projects/margonem-multi-bot/slide4.jpg"
-        img_2="../../static/img/projects/margonem-multi-bot/slide1.jpg"
-        project_route="/margonem-multi-bot"
-        project_live_link="https://margonembot.wordpress.com/"
-        project_code_link="https://github.com/venglas/margonem-bot"
-      >
-        <ProjectTag type="closed">desolate</ProjectTag>
-      </SingleProject>
-
-      <SingleProject
         project_name="Badoo spam bot"
         project_description="Jest to swego rodzaju spam bot jak i match bot pozwalający odfiltrować osoby, z którymi nie chcemy zostać połączeni."
         img_1="../../static/img/projects/badoo-spam-bot/slide4.jpg"
@@ -65,31 +90,6 @@
         project_code_link="https://github.com/venglas/badoo_bot"
       >
         <ProjectTag type="closed">desolate</ProjectTag>
-      </SingleProject>
-
-      <SingleProject
-        project_name="NBA Hall Of Hate"
-        project_description="Aplikacja webowa mająca na celu zbieranie opini, oraz w pewnym sensie gra społeczna jak i sieć łącząca fanów NBA :)"
-        img_1="../../static/img/projects/nba-hall-of-hate/slide1.jpg"
-        img_2="../../static/img/projects/nba-hall-of-hate/slide2.jpg"
-        project_route="/nba-hall-of-hate"
-        project_live_link="https://lucid-wright-54f76e.netlify.com"
-        project_code_link="https://github.com/venglas/nba-hall-of-hate-front"
-        project_code_link2="https://github.com/venglas/nba-hall-of-hate"
-      >
-        <ProjectTag type="inProgress">in progress</ProjectTag>
-      </SingleProject>
-
-      <SingleProject
-        project_name="IG Helper Tool"
-        project_description="IG Helper Tool to narzędzie, które pozwoli Ci łatwiej zarządzać Twoim profilem na portalu instagram oraz zdobyć większą ilość osób, które obserwują Twój profil."
-        img_1="../../static/img/projects/ig-helper-tool/slide1.jpg"
-        img_2="../../static/img/projects/ig-helper-tool/slide2.jpg"
-        project_route="/ig-helper-tool"
-        project_live_link="https://github.com/venglas/ig-helper"
-        project_code_link="https://github.com/venglas/ig-helper"
-      >
-        <ProjectTag type="inProgress">in progress</ProjectTag>
       </SingleProject>
 
       <!-- todo: add project refactorized and rewritied recrutation map application, new application based on map -->
